8560541fa64375d9d85c6c49d8b42fa641e974ca
Tuanle-dev <tuanhay1232@gmail.com>
2025-12-14 22:39:28 +0700

    Day 2: check pass or fail using if else

---
+++ day02_grade_checker.py
@@ -0,0 +1,18 @@
+"""toan = float(input("nhập điểm toán: "))
+van = float(input("nhập điểm văn: "))
+diem_trung_binh= (toan + van)/2
+print (f"điểm trung bình: {diem_trung_binh}")
+if diem_trung_binh >=5:
+    print (" đậu")
+else:
+    print ("rớt")
+"""
+
+math = float(input("enter your math score: "))
+english = float(input("enter your english score: "))
+average_score = (math + english)/2
+print (f"average score: {average_score}")
+if average_score >= 5:
+    print ("result: pass")
+else:
+    print ("result: fail")
