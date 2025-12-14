b8af9366fb3ac5d5dc9e1584961fa87850a41af9
Tuanle-dev <tuanhay1232@gmail.com>
2025-12-14 23:38:52 +0700

    Day 5: sum numbers from 1 to n using loop

--- day05_sum_1_to_n.py
+++ day05_sum_1_to_n.py
@@ -1,8 +1,17 @@
-number = int(input("Enter the number: "))
+"""number = int(input("Enter the number: "))
 if number <= 0:
     print("The number cannot be less than 0.")
 total = 0
 for i in range (1, number+1):
     total = total + i
-print(f"total: {total}")
+print(f"total: {total}")"""
 
+number = int(input("Enter the number: "))
+if number <= 0:
+    print("The number cannot be less than 0.")
+total = 0
+i = 1
+while i <= number:
+    total = total + i
+    i += 1
+print (f"total: {total}")
