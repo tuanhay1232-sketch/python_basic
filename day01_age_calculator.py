d63c0b04fdccc4fe70fd505b7199a59c64c2777e
Tuanle-dev <tuanhay1232@gmail.com>
2025-12-14 22:30:20 +0700

    Day 1: calculate age with Python

---
+++ day01_age_calculator.py
@@ -0,0 +1,11 @@
+""""ten = input("vui lòng nhập tên của bạn:  ")
+yrs = input("vui lòng nhập năm sinh: ")
+today_yrs = input ("vui lòng nhập năm nay: ")
+tuoi= int(today_yrs) - int(yrs)
+print("tên",ten, "tuổi",tuoi)"""
+
+name = input("please enter your name: ")
+birth_year= int(input(("please enter your birth year: ")))
+year= int(input(("please enter today year: ")))
+age = year - birth_year
+print (f"name:{name} | age:{age} ")
