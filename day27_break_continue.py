0a1f8d7b1f4071200cf5849dc3afd36b3f6b6093
Tuanle-dev <tuanhay1232@gmail.com>
2026-01-12 09:31:10 -0500

    day27-break-continue

---
+++ day27_break_continue.py
@@ -0,0 +1,15 @@
+#1
+"""
+number = int(input("Enter a number: "))
+for i in range (1, number + 1):
+    if i == 6:
+        break
+    print(i)
+"""
+#2
+"""number = int(input("Enter a number: "))
+for i in range (1, number + 1):
+    if i ==5:
+        continue
+    print(i)
+"""    
